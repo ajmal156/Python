4871d1f49bf7bfb4daf9e440716c4c4902d13303
ajmal <ma9156899@gmail.com>
2024-07-28 20:56:39 +0500

    simple program in python

---
+++ pactice_py.py
@@ -0,0 +1,9 @@
+print("THE FIRST DAY IN PYTHON")
+print("HELLO WORLD")
+print("HY HARRY")
+print(5*6)
+print("FIND THE NUMBER")
+
+def chai(hy):
+    print(hy)
+chai("THE FIRST EXPERINCE IN PYTHON")
