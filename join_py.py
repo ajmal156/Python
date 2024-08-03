5048fb8e3d900b5a1466734423441b3d3bf94e5c
ajmal <ma9156899@gmail.com>
2024-08-03 21:45:28 +0500

    are convert file to string

---
+++ join_py.py
@@ -0,0 +1,13 @@
+my =["my" ,"def" ,"but" ,"book" ,"name"]
+print(my)
+print("-".join(my))
+print(my[:-1])
+(print)(len(my))
+(print)(my.count("def"))
+num ="123456789"
+print(num[0:8])
+print(int(90.7989) + float(79))
+print("chai" + "code")
+x=2
+y=4
+print(x<y)
