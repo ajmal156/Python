197568f7b1a38a44e96469fd2870ebefd5ad0101
ajmal <ma9156899@gmail.com>
2024-08-03 21:40:14 +0500

    are command for string

---
+++ practic_py.py
@@ -0,0 +1,32 @@
+my_first ="my name is ajmal"
+print(len(my_first))
+print(my_first[:7])
+print(my_first[0:])
+print("hy,\"my name is ajmal\"")
+chai_chra ="lemon tea"
+print(chai_chra.upper())
+print(chai_chra.lower())
+print(chai_chra.find("lemon"))
+hy ="      hello world"
+print(hy.strip()) 
+print(len(hy))
+print(hy.split())
+my_first ="Name"
+chai_chra ="ajmal"
+order ="my {} is {}"
+print(order)
+print(order.format(my_first , chai_chra))
+print(len(order))
+print(order.count("name"))
+my ="genger tea"
+print(my)
+print(my.replace("genger" ,"lemon"))
+print("".join(my))
+there =["hy" ,"hello" ,"world" ,"famous" ,"get"]
+print(there)
+match =r"chai\norcode"
+print(match)
+hammy =r":c\user\practic_py"
+print(hammy)
+hy  ="masala Chai"
+print("masala" in "chai")
