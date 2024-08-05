d5705e0991dcb5f3e3de7e9c1254d3c98146226b
ajmal <ma9156899@gmail.com>
2024-08-05 14:27:27 +0500

    Syntax of file and string.

---
+++ srtip_py.py
@@ -0,0 +1,38 @@
+match_chra ="the first character of python. "
+print(match_chra.upper())
+name_char ="hello world"
+print(name_char.upper())
+print(name_char.count("hello"))
+from_char ="python"
+character =2
+order ="we have {} {} book."
+print(order.format(character ,from_char))
+print(len(order))
+get_char ="   HELLO WORLD."
+print(get_char.strip())
+print("There are a,\"paragraph in python program to desktop.\"")
+print(int(65.89) + float(67))
+print(get_char.replace("WORLD" ,"chai").strip())
+print(r"c:\user\strip_py.py\desktop")
+print("HY,\n\"MY name is ajmal\"")
+name_char =["python" ,"computer" ,"physics"]
+print(name_char[0:])
+print("-".join(name_char))
+print(len(name_char))
+print(name_char.insert(1 ,"html"))
+print(name_char)
+name_char[0:2] =["hello" ,"world"]
+print(name_char)
+name_char[2:2] =["python" ,"program"]
+print(name_char)
+print(match_chra.split())
+print(len(match_chra))
+cube_num =[y**3 for y in range(0,12)]
+print(cube_num)
+ma_chra =["pay" ,"pub" ,"but"]
+print(ma_chra)
+ma_chra.remove("pub")
+print(ma_chra)
+print(len(ma_chra))
+      
+    
