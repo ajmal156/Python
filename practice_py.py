90120bc001f0affdb28bcb79b8fb5191154ea9d1
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-08-05 18:57:49 +0500

    raw and range

---
+++ practice_py.py
@@ -0,0 +1,24 @@
+num_char =[y**3 for y in range(12)]
+print(num_char)
+hy_char =["hello" ,"world" ,"work" ,"python"]
+print(hy_char)
+print(hy_char.remove("hello"))
+print(len(hy_char))
+print(hy_char.count("world"))
+hy_char[1:3] =["html" ,"code"]
+print(hy_char)
+print("There,\"are most programare use in python.\"")
+print(hy_char[2])
+print(hy_char)
+pra_char =["fit" ,"get" ,"riple"]
+print(pra_char)
+pra_char.insert(1, "java")
+print(pra_char)
+pra_char_copy =pra_char.copy()
+print(pra_char_copy)
+pra_char_copy.insert(3 ,"sorcecode")
+print(pra_char_copy)
+pra_char_copy.remove("fit")
+print(pra_char_copy)
+pra_char_copy[0:2] =[]
+print(pra_char_copy)
