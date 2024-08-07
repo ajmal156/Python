4e5c7669857404e30435a9b8d1f72acbf14757be
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-08-07 19:46:22 +0500

    join form syntax

--- join_py.py
+++ join_py.py
@@ -1,13 +1,40 @@
-my =["my" ,"def" ,"but" ,"book" ,"name"]
-print(my)
-print("-".join(my))
-print(my[:-1])
-(print)(len(my))
-(print)(my.count("def"))
-num ="123456789"
-print(num[0:8])
-print(int(90.7989) + float(79))
-print("chai" + "code")
-x=2
-y=4
-print(x<y)
+char_type  ="Today the weather are most beautiful weather."
+print(char_type.upper())
+print(char_type.count("weather"))
+print(len(char_type))
+print(char_type[5:])
+print(char_type.split())
+print("".join(char_type))
+fet_type =("THERE,\"are the most beautiful place of the world\"")
+print(fet_type)
+hy ="  Hello. "
+print(hy.strip())
+get_type =["hy" ,"my" ,"name" ,"is" ,"ajmal"]
+print(get_type)
+print(len(get_type))
+print(get_type)
+print("_".join(get_type))
+print(get_type[:-1])
+print(r"c:user\pdf\join_py.py\desktop")
+jion_num=[y**2 for y in range(6)]
+print(jion_num)
+print(get_type.clear())
+me_char ={"There":"are" ,"python":"java" ,"get":"me"}
+print(me_char["There"])
+print(me_char.get("python"))
+me_char["from"] ="java"
+print(me_char)
+print(me_char.pop("There"))
+print(me_char.popitem())
+print(me_char)
+me_char_copy =me_char.copy()
+print(me_char_copy)
+me_char_copy["so"] ="thanks"
+print(me_char_copy)
+for dict in me_char:
+    print(dict, me_char[dict])
+    cube_type ={y:y**2 for y in range(6)}
+    print(cube_type)
+    
+
+
