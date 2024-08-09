0ce57456bcdfd1906d08b09419c423df87d2cb9d
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-08-09 20:48:57 +0500

    age

---
+++ 1_solution.py
@@ -0,0 +1,10 @@
+age =89
+
+if age < 15:
+    print("children")
+elif age <30:
+    print("Young")
+elif age <60:
+    print("Adult")
+else:
+   print ("Senior")            
---
+++ type_py.py
@@ -0,0 +1,16 @@
+hy_char =("there" ,"are" ,"throw")
+print(hy_char[-1])
+print(hy_char[2])
+print(len(hy_char))
+get_char =("character" ,"name" ,"character")
+print(get_char)
+all_num =hy_char +get_char
+print(all_num)
+print(type(hy_char))
+print(all_num.count("character"))
+print(hy_char)
+the_type =("get" ,"meth" ,"hy")
+(but,for ,there) ="join_type"
+print(for)
+
+
