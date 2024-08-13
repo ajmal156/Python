84804bff1720723f46faf9c7c97617703eeff7cb
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-08-13 19:55:11 +0500

    weak activity

---
+++ 5_solution.py
@@ -0,0 +1,20 @@
+weake =input("ENTER THE DAY PLAN:")
+
+if weake =="Sunday":
+    activity="Rest day"
+elif weake=="Monday":
+    activity="Read the book"
+elif weake=="Tuesday":
+    activity="Read today work"
+elif weake=="Wnesday":
+    activity="Runing the ground"
+elif weake=="Thusday":
+    activity="Today the wake"
+elif weake=="Friday":
+    activity="Go the pray for five time"
+elif weake=="Saturday":
+    activity="Play the game"                
+else:
+    print("sorry We are not avalibal.")
+
+print("Activity:" ,activity)    
