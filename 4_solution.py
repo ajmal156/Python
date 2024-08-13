99aa299c89038fb83f6116e2440ee8e735944820
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-08-13 19:54:16 +0500

    condition for apple

---
+++ 4_solution.py
@@ -0,0 +1,12 @@
+fruit =input("ENTER THE FRUIT TYPE:")
+color =input("ENTER THE FRUIT COLOR:")
+
+if fruit=="apple":
+    if color=="yellow":
+        print("Good ripe")
+    if color=="purple":
+        print("On ripe")
+    if color =="Broun":
+        print("Over ripe")        
+else:
+    print("Sorry are not avalibal")        
