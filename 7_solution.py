5cb7fe00d03c321d402950f16c7a594161ff5a41
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-08-13 19:57:31 +0500

    order for tea

---
+++ 7_solution.py
@@ -0,0 +1,8 @@
+extra_size= input("ENTER THE COFFE SIZE:")
+extra_short= True
+if extra_short:
+    chai=extra_size +" coffe is an extra short."
+else:
+    chai=extra_size +"Coffe"
+
+print("ORDER:" ,chai)    
