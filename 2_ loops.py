9b8a2bee4a01481ec6934decd748f2e54ef2c61c
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-08-17 20:41:57 +0500

    even number with for loops

---
+++ 2_ loops.py
@@ -0,0 +1,10 @@
+number =int (input("Enter the number:"))
+even_num =0
+
+for num in range(1 ,number +1):
+    if num % 2 ==0:
+        even_num +=1
+
+print("Final rueult of even number is:" ,even_num)
+
+
