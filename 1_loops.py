7e4f0ad5c9de6d17f1d8cda7e284bb7eb02768df
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-08-17 20:41:05 +0500

    positive count number

---
+++ 1_loops.py
@@ -0,0 +1,8 @@
+number =[1,2,-3,4,-5,-6,7,-8,9,-10]
+positive_count_number =0
+
+for num in number:
+    if num >1:
+        positive_count_number+=1
+
+print("Positive count number is:" ,positive_count_number)        
