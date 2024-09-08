ff0043d347c966dbb5b5f5a0b6997168438e87da
MUHAMMAD AJMAL <ma9156899@gmail.com>
2024-09-08 19:54:11 +0500

    basic oops

---
+++ oop.solution.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self ,model ,brand):
+        self.model =brand
+        self.brand =model
+
+my_car =Car("Toyota" ,"GLI")
+print(my_car.brand)
+print(my_car.model)
+
+my_new_car =Car("Honda" ,"Civic")
+print(my_new_car.brand)
+print(my_new_car.model)
+    
